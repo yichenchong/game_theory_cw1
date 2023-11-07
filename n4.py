3f973f8278dab3e19141d2e97421df63f0953891
Yi Chen Chong <ycc21@ic.ac.uk>
2023-11-07 23:54:42 +0000

    added benchmarking for n4

--- n4.py
+++ n4.py
@@ -1,9 +1,11 @@
 from z3 import *
 import pathlib
-
+import timeit
 
 s = Solver()
 
+libname = pathlib.Path().absolute() / "validator/validator.so"
+c_lib = ctypes.CDLL(libname)
 
 def main():
     a, b, c, d = [Int(chr(ord('a') + i)) for i in range(4)]
@@ -17,24 +19,25 @@ def main():
     s.add(d - c < 2)
     s.add(c - b < 6)
 
-    map_solutions(dv)
+    map_solutions(dv, verify)
 
 
 def map_solutions(dv: list, f: Function = print):
     solver = s.__deepcopy__()
     while solver.check() == sat:
-        f([solver.model()[i] for i in dv])
+        f([solver.model()[i].as_long() for i in dv])
         solver.add(Or(*[i != solver.model()[i] for i in dv]))
 
+
 def verify(solution: list):
+    start_time = timeit.default_timer()
     R = 10
     N = 4
-    libname = pathlib.Path().absolute() / "validator/validator.so"
-    c_lib = ctypes.CDLL(libname)
     arr_type = ctypes.c_int * R
     c_lib.verify_list_repr.argtypes = [arr_type, ctypes.c_int, ctypes.c_int]
     if c_lib.verify_list_repr(arr_type(*solution), N, R):
         print(solution)
+    t = timeit.default_timer() - start_time
 
 if __name__ == "__main__":
     main()
